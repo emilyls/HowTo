05b4ddef356977b7f0c1aefabf53cfe7892dbde9
emilyls <snyderem@oregonstate.edu>
2015-11-25 15:57:38 -0800

    using handlebars

--- ParkApp.js
+++ ParkApp.js
@@ -1,13 +1,17 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var bodyParser = require('body-parser');
 
-app.set('port', 3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5000);
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'Form.html'));
-
 // if(req.body['All Parks']){
     var url = 'http://oregonstateparks.org/data/index.cfm/parks';
     var options = {
